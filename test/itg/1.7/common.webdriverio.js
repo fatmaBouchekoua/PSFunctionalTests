5ea15e56aa6730df9eec4f3821427c4f0b75a162
fatmaBouchekoua <fatma.bouchekoua@prestashop.com>
2017-10-04 18:03:18 +0100

    Add element xpath and new commands

--- test/itg/1.7/common.webdriverio.js
+++ test/itg/1.7/common.webdriverio.js
@@ -116,6 +116,24 @@ module.exports = {
     take_screenshot: function (done) {
         client.saveScreenshot(__dirname + '/screenshots/' + client.desiredCapabilities.browserName + '_exception' + '_' + global.date_time + '_' + global.fctname + '.png');
     },
+    getCustomDate: function (numberOfDay) {
+        var today = new Date();
+        today.setDate(today.getDate() + numberOfDay);
+        var dd = today.getDate();
+        var mm = today.getMonth() + 1; //January is 0!
+        var yyyy = today.getFullYear();
+
+        if (dd < 10) {
+            dd = '0' + dd;
+        }
+
+        if (mm < 10) {
+            mm = '0' + mm;
+        }
+
+        today = yyyy + '-' + mm + '-' + dd;
+        return today;
+    },
     initMocha: function () {
         this.timeout(900000000);
         this.slow(45000);
